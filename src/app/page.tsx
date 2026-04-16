3f4dbe2e6859415d44cb2282c055e19c15ccf2fb
BitWizCoder <mhmdnoman01@gmail.com>
2026-04-17 04:18:23 +0600

    feat: added sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Hero from "@/components/hero";
+import Team from "@/components/team";
+import Testimonials from "@/components/testimonials";
 
 export default function Home() {
   return (
     <div className="">
       <Hero />
+      <Team />
+      <Testimonials />
     </div>
   );
 }
